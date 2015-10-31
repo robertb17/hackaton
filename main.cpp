466ac41c9a973f81672ece658301e1981501434b
Popescu Alexandru <alex95cristi@yahoo.com>
2015-10-31 20:00:45 +0200

    main and makefile first draft

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <cstdio>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <cstring>
+
+int main(int argc, char *argv[]) {
+
+	int sockfd, portno, n;
+	char buffer[256];
+	struct sockaddr_in serv_addr;
+	struct hostent *server;
+
+	if(argc < 3) {
+		fprintf(stderr, "usage %s hostname port", argv[0]);
+	}
+
+	// port number of the client
+	portno = atoi(argv[2]);
+
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if(sockfd < 0)
+		fprintf(stderr, "ERROR opening socket");
+	
+	// name of the host
+	server = gethostbyname(argv[1]);
+	if (server == NULL) {
+		fprintf(stderr, "ERRROR, no such host");
+	}
+
+	memset( &serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	bcopy((char *)server->h_addr,
+	      (char *)&serv_addr.sin_addr.s_addr,
+		        server->h_length);
+	serv_addr.sin_port = htons(portno);
+
+	if (connect(sockfd,(struct sockaddr *) &serv_addr,
+		  sizeof(serv_addr)) < 0)
+		fprintf(stderr, "ERROR, conection failed");
+
+
+	return 0;
+}
+
