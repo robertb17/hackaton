976e3585923f83f53cf5a5ae1a4eec0c6eaa42ba
Robert Badea <robert.badea95@gmail.com>
2015-10-31 22:26:41 +0200

    alexxino retardat

---
+++ data_str.h
@@ -0,0 +1,18 @@
+
+
+struct player {
+	short player_id;
+	short pos_x;
+	short pos_y;
+};
+
+struct game_board {
+	bool is_wall;
+	short danger_level; // 0 -> 100
+	char on_fire;
+	char has_bomb;
+};
+
+void update_state(game_board );
+bool get_info(char*, int&, int&, int&, int&, int&);
+game_board** trans_input(char*, player*, int, int);
