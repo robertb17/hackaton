bd64fe07c72eac93e5fa72bd2f92630bd3a2f2fe
Robert Badea <robert.badea95@gmail.com>
2015-10-31 22:16:08 +0200

    noob

--- data_str.h
+++ data_str.h
@@ -15,4 +15,4 @@ struct game_board {
 
 void update_state(game_board );
 bool get_info(char*, int&, int&, int, int&, int&);
-game_board** trans_input(char*, player**, int, int);
+game_board** trans_input(char*, player*, int, int);
